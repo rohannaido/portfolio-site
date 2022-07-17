6ba88907b57baacc0e4679ccf822176137869b31
rohannaido <87189343+rohannaido@users.noreply.github.com>
2022-07-17 19:05:19 +0530

    feat: Added projects and blogs

---
+++ src/data.js
@@ -0,0 +1,39 @@
+const projectList = [
+    {
+        title: "Fullstack Ecommerce App",
+        description: "A Fullstack ecommerce app which was built using ReactJS for the frontend and ExpressJS for the its backend API which is integerated with MongoDB Database.",
+        projectLink: "https://rohannaido.github.io/ecommerce-app/",
+        codeLink: "https://github.com/rohannaido/ecommerce-app"
+    },
+    {
+        title: "Wheres waldo",
+        description: "A Game with ReactJS in the frontend and Firebase as backend. User has to locate characters in the search image and time will be logged in the backend.",
+        projectLink: "https://rohannaido.github.io/wheres-waldo/",
+        codeLink: "https://github.com/rohannaido/wheres-waldo"
+    },
+    {
+        title: "Shopping Cart",
+        description: "Simple ReactJS application with React Routing implementation and add to cart functionality.",
+        projectLink: "https://rohannaido.github.io/shopping-cart/",
+        codeLink: "https://github.com/rohannaido/shopping-cart",
+    },
+    {
+        title: "CV Project",
+        description: "ReactJS project to implement useState to update and Build CV.",
+        projectLink: "https://rohannaido.github.io/cv-project/",
+        codeLink: "https://github.com/rohannaido/cv-project",
+    },
+    {
+        title: "Memory Card Game",
+        description: "ReactJS Web Game to test memory power by remembering selected cards.",
+        projectLink: "https://rohannaido.github.io/memory-card-game/",
+        codeLink: "https://github.com/rohannaido/memory-card-game",
+    }
+];
+
+const blogList = [];
+
+module.exports = {
+    projectList: projectList,
+    blogList: blogList
+}
---
+++ src/index.js
@@ -0,0 +1,35 @@
+const {projectList} = require('./data')
+
+const projectListElement = document.querySelector(".project-list");
+
+for(let i = 0; i < projectList.length; i++){
+    const item = projectList[i];
+    const listItem = document.createElement("li");
+    listItem.classList.add("project-item");
+    
+    const projectTitle = document.createElement("h3");
+    projectTitle.textContent = item.title;
+    
+    const projectDesc = document.createElement("p");
+    projectDesc.textContent = item.description;
+
+    const siteLink = document.createElement("a");
+    siteLink.setAttribute("href", item.projectLink)
+    siteLink.classList.add("project-link")
+    siteLink.setAttribute("target", "_blank");
+    siteLink.textContent = "Visit Site"
+    
+    const codeLink = document.createElement("a");
+    codeLink.setAttribute("href", item.codeLink)
+    codeLink.classList.add("project-link")
+    codeLink.setAttribute("target", "_blank");
+    codeLink.textContent = "View Code"
+
+    listItem.appendChild(projectTitle);
+    listItem.appendChild(projectDesc);
+    listItem.appendChild(siteLink);
+    listItem.appendChild(codeLink);
+
+    projectListElement.appendChild(listItem);
+}
+
