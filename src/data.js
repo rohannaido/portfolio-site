f32244dc24f68e2eaad1cd63d150e5a6ef4d49bd
rohannaido <87189343+rohannaido@users.noreply.github.com>
2022-08-01 08:38:30 +0530

    feat: added more projects and their features and description.

--- src/data.js
+++ src/data.js
@@ -1,5 +1,12 @@
 const projectList = [
     {
+        title: "Booking App",
+        description: "A booking app where users can book hotels in Indian cities. It is integrated with Firebase to fetch hotel data as per city name. Users can filter and sort hotels results by price and ratings. Hotel Images are displayed in a carousel form. All the components are built using pure CSS and in-house.",
+        img:"./images/booking-app-screenshot.png",
+        projectLink: "https://rohannaido.github.io/booking-app/",
+        codeLink: "https://github.com/rohannaido/booking-app"
+    },
+    {
         title: "Fullstack Ecommerce App",
         description: "A Fullstack ecommerce app which was built using ReactJS for the frontend and ExpressJS for the its backend API which is integerated with MongoDB Database. The backend API is hosted on Heroku and consumes MongoDB's cloud database.",
         img:"./images/ecommerce-screenshot.png" ,
@@ -7,36 +14,33 @@ const projectList = [
         codeLink: "https://github.com/rohannaido/ecommerce-app"
     },
     {
-        title: "Booking App",
-        description: "A booking app where hotel bookings can be made. It is integrated with Firebase to fetch hotel data as per city name.",
-        img:"./images/booking-app-screenshot.png",
-        projectLink: "https://rohannaido.github.io/booking-app/",
-        codeLink: "https://github.com/rohannaido/booking-app"
+        title: "Ecommerce Admin App",
+        description: "Ecommerce app which was built using ReactJS for the frontend and consumes NodeJS backend API which is integerated with MongoDB Database. The backend API is hosted on Heroku and consumes MongoDB's cloud database. This App enable admin privilege users to add, delete, and edit products on the Ecommerce store.",
+        img:"./images/ecommerce-admin.png" ,
+        projectLink: "https://rohannaido.github.io/ecommerce-admin/",
+        codeLink: "https://github.com/rohannaido/ecommerce-admin"
+    },
+    {
+        title: "Video App",
+        description: "This is a video library application where users can watch videos, add videos to watch Later, Like videos and access and clear their watch history.",
+        img:"./images/video-app.png" ,
+        projectLink: "https://rohannaido.github.io/video-app/",
+        codeLink: "https://github.com/rohannaido/video-app"
+    },
+    {
+        title: "Notes App",
+        description: "A notes app inspired by Google Notes design. Users can create, edit and delete their notes. Every user get their own notes storage.",
+        img:"./images/notes-app.png" ,
+        projectLink: "https://rohannaido.github.io/video-app/",
+        codeLink: "https://github.com/rohannaido/video-app"
     },
     {
         title: "Wheres waldo",
         description: "A Game with ReactJS in the frontend and Firebase as backend. User has to locate characters in the search image and time will be logged in the backend.",
+        img:"./images/wheres-waldo.png",
         projectLink: "https://rohannaido.github.io/wheres-waldo/",
         codeLink: "https://github.com/rohannaido/wheres-waldo"
     },
-    {
-        title: "Shopping Cart",
-        description: "Simple ReactJS application with React Routing implementation and add to cart functionality.",
-        projectLink: "https://rohannaido.github.io/shopping-cart/",
-        codeLink: "https://github.com/rohannaido/shopping-cart",
-    },
-    {
-        title: "CV Project",
-        description: "ReactJS project to implement useState to update and Build CV.",
-        projectLink: "https://rohannaido.github.io/cv-project/",
-        codeLink: "https://github.com/rohannaido/cv-project",
-    },
-    {
-        title: "Memory Card Game",
-        description: "ReactJS Web Game to test memory power by remembering selected cards.",
-        projectLink: "https://rohannaido.github.io/memory-card-game/",
-        codeLink: "https://github.com/rohannaido/memory-card-game",
-    }
 ];
 
 const blogList = [];
